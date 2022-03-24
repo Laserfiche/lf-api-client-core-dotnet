ffea6a09ae9671f3677d11099c7936766a4a6646
Jason Jia <jason.jia@laserfiche.com>
2022-03-24 17:26:06 -0400

    Add integration tests

---
+++ tests/integration/BaseTest.cs
@@ -0,0 +1,31 @@
+﻿using Laserfiche.Oauth.Api.Client;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Laserfiche.OAuth.Client.ClientCredentials.IntegrationTest
+{
+    public class BaseTest
+    {
+        public ClientCredentialsOptions Configuration { get; set; }
+
+        public BaseTest()
+        {
+            ExtractConfigurationFromJson();
+        }
+
+        private void ExtractConfigurationFromJson()
+        {
+            string testingConfig;
+            try
+            {
+                testingConfig = File.ReadAllText(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "DEV_A_CA.json"));
+            }
+            catch (Exception)
+            {
+                throw new Exception("Cannot load TestingConfig.json");
+            }
+            Configuration = JsonConvert.DeserializeObject<ClientCredentialsOptions>(testingConfig);
+        }
+    }
+}
---
+++ tests/integration/GetAccessTokenTest.cs
@@ -0,0 +1,45 @@
+﻿using Laserfiche.Oauth.Api.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Laserfiche.OAuth.Client.ClientCredentials.IntegrationTest
+{
+    [TestClass]
+    public class GetAccessTokenTest : BaseTest
+    {
+        [TestMethod]
+        public async Task GetAccessToken()
+        {
+            // Initialize an instance of the handler
+            ClientCredentialsHandler handler = new ClientCredentialsHandler(Configuration);
+
+            // Get access token for that application
+            var accessToken = await handler.GetAccessToken();
+            Assert.IsNotNull(accessToken);
+        }
+
+        [TestMethod]
+        public async Task GetAccessAsync_WrongBaseAddress()
+        {
+            // Initialize an instance of the handler
+            ClientCredentialsHandler handler = new ClientCredentialsHandler(Configuration);
+            handler.Configuration.BaseAddress = "https://some.random.string"; // Wrong base address.
+
+            // Expect failed attempt to get access token since the base address is wrong
+            await Assert.ThrowsExceptionAsync<HttpRequestException>(async () => await handler.GetAccessToken());
+        }
+
+        [TestMethod]
+        public async Task GetAccessAsync_WrongServicePrincipalKey()
+        {
+            // Initialize an instance of the handler
+            ClientCredentialsHandler handler = new ClientCredentialsHandler(Configuration);
+            handler.Configuration.ServicePrincipalKey = "a wrong service principal key";
+
+            // Expect the retrieval of access token to fail due to incorrect service principal key
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () => await handler.GetAccessToken());
+        }
+    }
+}
