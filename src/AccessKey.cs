4c4e9b6e91730e4058c3e1420b95128ff54937dd
Jason Jia <jason.jia@laserfiche.com>
2022-05-05 16:26:04 -0400

    Align with the TS counterpart

---
+++ src/AccessKey.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace Laserfiche.Oauth.Api.Client
+{
+    public class AccessKey
+    {
+        [JsonProperty("customerId")]
+        public string CustomerId { set; get; }
+
+        [JsonProperty("domain")]
+        public string Domain { set; get; }
+
+        [JsonProperty("client_id")]
+        public string ClientId { set; get; }
+
+        [JsonProperty("accessKey")]
+        public JsonWebKey Jwk { set; get; }
+    }
+}
--- src/ClientCredentialsOptions.cs
+++ src/ClientCredentialsOptions.cs
@@ -1,23 +1,9 @@
-﻿using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json;
-
-namespace Laserfiche.Oauth.Api.Client
+﻿namespace Laserfiche.Oauth.Api.Client
 {
     public class ClientCredentialsOptions
     {
-        [JsonProperty("customerId")]
-        public string CustomerId { set; get; }
-
-        [JsonProperty("domain")]
-        public string Domain { set; get; }
-
-        [JsonProperty("client_id")]
-        public string ClientId { set; get; }
-
-        [JsonProperty("servicePrincipalKey")]
         public string ServicePrincipalKey { set; get; }
 
-        [JsonProperty("accessKey")]
-        public JsonWebKey Jwk { set; get; }
+        public AccessKey AccessKey { set; get; }
     }
 }
