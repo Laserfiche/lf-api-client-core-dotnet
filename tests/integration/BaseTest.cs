59b2c3f908c21a7fbf953a9dc2ebc9587ec01138
Jason Jia <jason.jia@laserfiche.com>
2022-05-06 15:48:33 -0400

    Use environment variable all the way

--- tests/integration/BaseTest.cs
+++ tests/integration/BaseTest.cs
@@ -9,51 +9,30 @@ namespace Laserfiche.OAuth.Client.ClientCredentials.IntegrationTest
     {
         public ClientCredentialsOptions Configuration { get; set; }
 
-        private readonly string ACCESS_KEY = "TEST_CONFIG_DEV_A_CA";
-
-        private readonly string SERVICE_PRINCIPAL = "TEST_CONFIG_SP_DEV_A_CA";
-
         public BaseTest()
         {
-            var servicePrincipal = Environment.GetEnvironmentVariable(SERVICE_PRINCIPAL);
-            if (servicePrincipal == null)
-            {
-                servicePrincipal = ReadJsonFromFile<ServicePrincipalConfig>(SERVICE_PRINCIPAL).Value;
-            }
-
-            AccessKey accessKey;
-            var accessKeyStr = Environment.GetEnvironmentVariable(ACCESS_KEY);
-            if (accessKeyStr == null)
-            {
-                accessKey = ReadJsonFromFile<AccessKey>(ACCESS_KEY);
-            } else
-            {
-                accessKey = JsonConvert.DeserializeObject<AccessKey>(accessKeyStr);
-            }
-
-            Configuration = new()
-            {
-                ServicePrincipalKey = servicePrincipal,
-                AccessKey = accessKey
-            };
+            SourceTestConfig("TestConfig");
         }
-        private static T ReadJsonFromFile<T>(string fileName)
+
+        private void SourceTestConfig(string fileName)
         {
-            string config;
-            try
-            {
-                config = File.ReadAllText(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, $"{fileName}.json"));
-            }
-            catch (Exception)
-            {
-                throw new Exception($"Cannot load {fileName}");
-            }
-            return JsonConvert.DeserializeObject<T>(config);
-        }
-    }
+            var path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, $"{fileName}.env");
+            var env = DotNetEnv.Env.Load(path);
 
-    internal class ServicePrincipalConfig
-    {
-        public string Value { get; set; }
+            Configuration = new();
+
+            foreach (var kv in env)
+            {
+                switch (kv.Key)
+                {
+                    case "SERVICE_PRINCIPAL_KEY":
+                        Configuration.ServicePrincipalKey = kv.Value;
+                        break;
+                    case "ACCESS_KEY":
+                        Configuration.AccessKey = JsonConvert.DeserializeObject<AccessKey>(kv.Value);
+                        break;
+                }
+            }
+        }
     }
 }
