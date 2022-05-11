02eb1c05e46b7514763b8b41e88961897dbb1a24
Jason Jia <jason.jia@laserfiche.com>
2022-05-11 10:30:50 -0400

    Add missing file

---
+++ src/Util/OAuthProblemDetails.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace Laserfiche.Oauth.Api.Client
+{
+    internal class OAuthProblemDetails
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("status")]
+        public int? Status { get; set; }
+
+        [JsonProperty("instance")]
+        public string Instance { get; set; }
+
+        [JsonProperty("operationId")]
+        public string OperationId { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Type}: {Title}";
+        }
+    }
+}
