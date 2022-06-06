c43b052b69c28a45102119703b2146bc5a3a8663
Jason Jia <jason.jia@laserfiche.com>
2022-06-06 16:31:10 -0400

    Only call decode once

--- tests/integration/BaseTest.cs
+++ tests/integration/BaseTest.cs
@@ -18,15 +18,13 @@ namespace Laserfiche.Api.Client.IntegrationTest
 
         private const string SpKeyVar = "DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY";
 
-        private bool HasBase64Decoded = false;
-
         public BaseTest()
         {
             TryLoadFromDotEnv(TestConfigFile);
             PopulateFromEnv();
         }
 
-        private void TryLoadFromDotEnv(string fileName)
+        private static void TryLoadFromDotEnv(string fileName)
         {
             var path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, fileName);
             if (File.Exists(path))
@@ -36,38 +34,24 @@ namespace Laserfiche.Api.Client.IntegrationTest
                     clobberExistingVars: true,
                     onlyExactPath: true
                 ));
-
-                // Decode ACCESS_KEY
-                DecodeAccessKeyAndUpdateEnvVar();
                 
                 System.Diagnostics.Trace.TraceWarning($"{fileName} found. {fileName} file should only be used in local developer computers.");
             }
             else
-            {
                 System.Diagnostics.Trace.WriteLine($"{fileName} not found.");
-            }
         }
 
-        private void DecodeAccessKeyAndUpdateEnvVar()
+        private static string DecodeBase64(string encoded)
         {
-            var base64Encoded = Environment.GetEnvironmentVariable(AccessKeyVar);
-            if (base64Encoded != null)
-            {
-                var decoded = Encoding.UTF8.GetString(Convert.FromBase64String(base64Encoded));
-                Environment.SetEnvironmentVariable(AccessKeyVar, decoded);
-                HasBase64Decoded = true;
-            }
+            return Encoding.UTF8.GetString(Convert.FromBase64String(encoded));
         }
 
         private void PopulateFromEnv()
         {
             ServicePrincipalKey = Environment.GetEnvironmentVariable(SpKeyVar);
             
-            if (!HasBase64Decoded)
-            {
-                DecodeAccessKeyAndUpdateEnvVar();
-            }
-            AccessKey = JsonConvert.DeserializeObject<AccessKey>(Environment.GetEnvironmentVariable(AccessKeyVar));
+            var accessKeyStr = DecodeBase64(Environment.GetEnvironmentVariable(AccessKeyVar));
+            AccessKey = JsonConvert.DeserializeObject<AccessKey>(accessKeyStr);
         }
     }
 }
