763d68834bd735d016d6048e81ca8acf9669d012
sxu1998 <103658932+sxu1998@users.noreply.github.com>
2022-08-04 16:43:43 -0400

    refactored accessKey object (#50)
    * refactored accessKey object
    
    * added a private static variable in the accessKey obj
    
    * refacotered basetest file
    
    * resolved comments
    
    * resolved remaining comment
    
    * updated function and added unit tests
    
    * renamed variable
    
    * updated prod package version
    
    * update to version 1.2.1
    
    * resolved comments
    
    * resolved failed test case

--- src/OAuth/AccessKey.cs
+++ src/OAuth/AccessKey.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
+using System;
+using System.Text;
 
 namespace Laserfiche.Api.Client.OAuth
 {
@@ -20,5 +22,23 @@ namespace Laserfiche.Api.Client.OAuth
 
         [JsonProperty("jwk")]
         public JsonWebKey Jwk { set; get; }
+
+        public static AccessKey CreateFromBase64EncodedAccessKey(string base64EncodedAccessKey)
+        {
+            if (string.IsNullOrWhiteSpace(base64EncodedAccessKey))
+            {
+                throw new ArgumentNullException(nameof(base64EncodedAccessKey));
+            }
+            var accessKeyStr = Encoding.UTF8.GetString(Convert.FromBase64String(base64EncodedAccessKey));
+            try
+            {
+                AccessKey accessKey = JsonConvert.DeserializeObject<AccessKey>(accessKeyStr);
+                return accessKey;
+            }
+            catch(JsonReaderException e)
+            {
+                throw new FormatException($"{nameof(base64EncodedAccessKey)} is not valid", e);
+            }
+        }
     }
 }
